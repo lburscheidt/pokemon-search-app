f73704d535a412dda5543c49f62e1ac08f1cbe4b
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-29 13:50:25 +0100

    feat: empty page contents between searches

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const card = document.querySelector("#card");
+const stats = document.querySelector("#stats");
+
 const searchInput = document.querySelector("#search-input");
 const searchBtn = document.querySelector("#search-button");
 const pokemonName = document.querySelector("#pokemon-name");
@@ -11,7 +14,6 @@ const defense = document.querySelector("#defense");
 const specialAttack = document.querySelector("#special-attack");
 const specialDefense = document.querySelector("#special-defense");
 const speed = document.querySelector("#speed");
-pokemonGet(45);
 
 const hpTextField = document.querySelector("#hp-text");
 const attackTextField = document.querySelector("#attack-text");
@@ -19,14 +21,13 @@ const defenseTextField = document.querySelector("#defense-text");
 const specialAttackTextField = document.querySelector("#special-attack-text");
 const specialDefenseTextField = document.querySelector("#special-defense-text");
 const speedTextField = document.querySelector("#speed-text");
-
 const pokemonImage = document.querySelector("#pokemon-image");
 
 function capitalizeFirstLetter(str) {
   return String(str).charAt(0).toUpperCase() + String(str).slice(1);
 }
 
-function pokemonGet(pokemon) {
+function pokeGet(pokemon) {
   let pokemonStr = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemon}`;
   console.log(
     fetch(pokemonStr)
@@ -34,6 +35,7 @@ function pokemonGet(pokemon) {
       .then((data) => {
         pokemonDataArray = data;
         console.log("Pokemon Data Array:", pokemonDataArray);
+
         /*card*/
         const nameValue = data.name;
         const idValue = data.id;
@@ -52,7 +54,8 @@ function pokemonGet(pokemon) {
           typeDiv.classList.add("type");
           types.appendChild(typeDiv);
         });
-        const img = data.sprites.front_default;
+        pokemonImage.src = data.sprites.front_default;
+
         /*stats*/
         const hpValue = data.stats[0].base_stat;
         const attackValue = data.stats[1].base_stat;
@@ -60,7 +63,6 @@ function pokemonGet(pokemon) {
         const specialAttackValue = data.stats[3].base_stat;
         const specialDefenseValue = data.stats[4].base_stat;
         const speedValue = data.stats[5].base_stat;
-
         /*text */
         const hpText = data.stats[0].stat.name.toUpperCase();
         const attackText = capitalizeFirstLetter(data.stats[1].stat.name);
@@ -89,11 +91,39 @@ function pokemonGet(pokemon) {
         specialAttack.innerText += specialAttackValue;
         specialDefense.innerText += specialDefenseValue;
         speed.innerText += speedValue;
-        pokemonImage.src = data.sprites.front_default;
-      }),
+      })
   );
 }
 
+function empty(element) {
+  while (element.firstElementChild) {
+    element.firstElementChild.innerHTML = "";
+  }
+}
+
 searchBtn.addEventListener("click", () => {
-  pokemonGet(searchInput.value);
+  pokemonName.innerText = "";
+  pokemonId.innerText = "";
+  weight.innerText = "";
+  height.innerText = "";
+  pokemonImage.src = "";
+  types.innerHTML = "";
+
+  hpTextField.innerText = "";
+  attackTextField.innerText = "";
+  defenseTextField.innerText = "";
+  specialAttackTextField.innerText = "";
+  specialDefenseTextField.innerText = "";
+  speedTextField.innerText = "";
+
+  hp.innerText = "";
+  attack.innerText = "";
+  defense.innerText = "";
+  specialAttack.innerText = "";
+  specialDefense.innerText = "";
+  speed.innerText = "";
+
+  console.log(searchInput.value);
+
+  pokeGet(searchInput.value);
 });
